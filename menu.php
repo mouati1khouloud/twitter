d9822f479bc84a30890f27e841cb87499fdd4b6a
mouati1khouloud <khouloudmouati123@gmail.com>
2019-02-28 15:00:55 +0100

    first

---
+++ menu.php
@@ -0,0 +1,18 @@
+      <br />
+   <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+     <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">Webslesson</a>
+     </div>
+     <ul class="nav navbar-nav navbar-right">
+      <li class="dropdown">
+       <a class="dropdown-toggle" data-toggle="dropdown" href="#"><?php echo $_SESSION['username']; ?>
+       <span class="caret"></span></a>
+       <ul class="dropdown-menu">
+        <li><a href="profile.php">Profile</a></li>
+        <li><a href="logout.php">Logout</a></li>
+       </ul>
+      </li>
+     </ul>
+    </div>
+   </nav>
